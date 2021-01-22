f21075a904e0253e16136b6a84a0247a8544d566
Liam Coffey <liamkohncoffey@gmail.com>
2021-01-22 13:49:03 +0100

    added in an hAPI to test GRPC ClientFactory

---
+++ AdventureWorks.GRPC/AdventureWorks.API/Controllers/AdventureWorksController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdventureWorks.GRPC;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AdventureWorks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdventureWorksController : ControllerBase
+    {
+        private readonly Greeter.GreeterClient _client;
+
+        public AdventureWorksController(Greeter.GreeterClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var reply = _client.SayHello(new HelloRequest
+            {
+                Name = "Liam Coffey"
+            });
+            return Ok(reply);
+        }
+    }
+}
