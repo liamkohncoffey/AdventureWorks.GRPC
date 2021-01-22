8d5807e3d12c18d299c0e700cdc7430b5565beb6
Liam Coffey <liamkohncoffey@gmail.com>
2021-01-22 11:27:46 +0100

    Completed tutoral

---
+++ AdventureWorks.GRPC/AdventureWorks.GRPC.Client/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace AdventureWorks.GRPC.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // This switch must be set before creating the GrpcChannel/HttpClient.
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            
+            Console.WriteLine("Sending unary call...");
+
+            var reply = await client.SayHelloAsync(new HelloRequest
+            {
+                Name = "Liam Coffey"
+            });
+            
+            Console.WriteLine("Unary response:" + reply.Message);
+            
+            Console.WriteLine("Sending request for server stream...");
+
+            using (var call = client.SayManyHellos(new HelloRequest{Name = "GreeterClient"}))
+            {
+                await foreach (var response in call.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine("New element from response stream:" + response.Message);
+                }
+            }
+            
+            Console.WriteLine("Sending client stream...");
+
+            var listOfNames = new List<string> {"John", "James", "Freddy", "David"};
+            
+            Console.WriteLine("Names about to be sent:" + string.Join(", ", listOfNames));
+
+            using (var call = client.SayHelloToLastRequest())
+            {
+                foreach (var name in listOfNames)
+                {
+                    await call.RequestStream.WriteAsync(new HelloRequest
+                    {
+                        Name = name
+                    });
+                }
+
+                await call.RequestStream.CompleteAsync();
+
+                Console.WriteLine("Response from client stream:" + (await call.ResponseAsync).Message);
+            }
+
+            Console.WriteLine("Sending bi-directional call...");
+
+            using (var call = client.SayHelloToEveryRequest())
+            {
+                foreach (var name in listOfNames)
+                {
+                    await call.RequestStream.WriteAsync(new HelloRequest
+                    {
+                        Name = name
+                    });
+                }
+                
+                await call.RequestStream.CompleteAsync();
+
+                await foreach (var response in call.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine("Individual item from bi-Directional call: " + response.Message);
+                }
+            }
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
--- AdventureWorks.GRPC/AdventureWorks.GRPC/Services/GreeterService.cs
+++ AdventureWorks.GRPC/AdventureWorks.GRPC/Services/GreeterService.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
-namespace AdventureWorks.GRPC
+namespace AdventureWorks.GRPC.Services
 {
     public class GreeterService : Greeter.GreeterBase
     {
@@ -22,6 +19,44 @@ namespace AdventureWorks.GRPC
             {
                 Message = "Hello " + request.Name
             });
+        }
+
+        public override async Task SayManyHellos(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                await responseStream.WriteAsync(new HelloReply
+                {
+                    Message = $"Hello {request.Name}, Number: {i}"
+                });
+            }
+        }
+
+        public override async Task<HelloReply> SayHelloToLastRequest(IAsyncStreamReader<HelloRequest> requestStream, ServerCallContext context)
+        {
+            var name = "";
+
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                name = request.Name;
+            }
+            
+            return  new HelloReply
+            {
+                Message = $"Hello {name}"
+            };
+        }
+
+        public override async Task SayHelloToEveryRequest(IAsyncStreamReader<HelloRequest> requestStream, IServerStreamWriter<HelloReply> responseStream,
+            ServerCallContext context)
+        {
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                await responseStream.WriteAsync(new HelloReply
+                {
+                    Message = $"Hello {request.Name}"
+                });
+            }
         }
     }
 }
